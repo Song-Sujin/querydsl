2e669c26ada1a5642c9fea6d68d6e96e46553cbb
Song-Sujin <ssj1744@naver.com>
2022-09-21 01:11:52 +0900

    querydsl 환경세팅 완료

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,22 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")   // 외래키 이름. 컬럼의 이름
+    private Team team;
+
+}
